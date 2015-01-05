4de5512d52b79004046abd6d8aa77c9f92b4f854
Benjamin <brpylko@gmail.com>
2015-01-05 12:38:46 -0500

    Fixed home directory

--- setup.sh
+++ setup.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-add-apt-repository ppa:byteit101/frc-toolchain &&
-apt-get update &&
-apt-get install frc-toolchain &&
 read -p "Enter your team number: " TEAMNO &&
 sed -i -e "s/TEAMNO/$TEAMNO/g" makefile.orig &&
 mv makefile.orig makefile
